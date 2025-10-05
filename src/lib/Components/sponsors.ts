db78ae6bc897df61e9cd570c713f61343255dedc
Thomas Zaußnig <44778289+razordb9@users.noreply.github.com>
2025-10-05 20:36:25 +0200

    sponsors component added

---
+++ src/lib/Components/sponsors.ts
@@ -0,0 +1,12 @@
+export const sponsors=[
+    {
+        name:"Uniqua",
+        logo:"uniqua.png",
+        link:"https://www.uniqua.at"
+    },
+    {
+        name:"Auto Rainer",
+        logo:"autorainer.png",
+        link:"https://www.auto-rainer.at"
+    }, 
+]
