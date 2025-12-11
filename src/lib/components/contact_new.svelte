2709e8de6fdb6e9746d1df77f2e41c104178ff23
Thomas Zaußnig <44778289+razordb9@users.noreply.github.com>
2025-12-11 22:55:16 +0100

    conctact css adapted

--- src/lib/components/contact_new.svelte
+++ src/lib/components/contact_new.svelte
@@ -4,27 +4,28 @@
    let form=$state<HTMLFormElement>();
 
 </script>
-
-<form bind:this={form} method="POST" use:enhance action="?/sendmail" name="contact">
-    <fieldset>
-        <label>Name (*) <input type="text" id="name" name="name"></label>
-        {#if form?.errors && form?.values.name}
-            <p class="error">{form?.errors["name"]?.errors}</p>
-        {/if} 
-        <label>Email (*)<input type="email" id="email" name="email" autocomplete="username" placeholder="example@domain.com" ></label>
-        {#if form?.errors && form?.values.email}
-            <p class="error">{form?.errors["email"]?.errors}</p>
-        {/if} 
-        <label>Text (*)<textarea id="message" name="message" rows="7"></textarea></label>    
-        {#if form?.errors && form?.values.message}
-            <p class="error">{form?.errors["message"]?.errors}</p>
-        {/if}     
-        <button type="submit">Absenden</button>
-        {#if form?.success}
-            <p style="color:green;">✅ Message sent successfully!</p>
-        {/if}
-        {#if form?.error}
-            <p style="color:red;">❌ {form.error}</p>
-        {/if}
-    </fieldset>
-</form>
+<div id="contactform">
+    <form bind:this={form} method="POST" use:enhance action="?/sendmail" name="contact">
+        <fieldset>
+            <label>Name (*) <input type="text" id="name" name="name"></label>
+            {#if form?.errors && form?.values.name}
+                <p class="error">{form?.errors["name"]?.errors}</p>
+            {/if} 
+            <label>Email (*)<input type="email" id="email" name="email" autocomplete="username" placeholder="example@domain.com" ></label>
+            {#if form?.errors && form?.values.email}
+                <p class="error">{form?.errors["email"]?.errors}</p>
+            {/if} 
+            <label>Text (*)<textarea id="message" name="message" rows="7"></textarea></label>    
+            {#if form?.errors && form?.values.message}
+                <p class="error">{form?.errors["message"]?.errors}</p>
+            {/if}     
+            <button type="submit">Absenden</button>
+            {#if form?.success}
+                <p style="color:green;">✅ Message sent successfully!</p>
+            {/if}
+            {#if form?.error}
+                <p style="color:red;">❌ {form.error}</p>
+            {/if}
+        </fieldset>
+    </form>
+</div>
